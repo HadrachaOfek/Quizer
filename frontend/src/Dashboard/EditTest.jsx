653150b96e1b4a0c910e67191b36c3a1247d00f4
Galperetz1 <galushy222@gmail.com>
2023-01-04 11:03:19 +0200

    start over

--- frontend/src/Dashboard/EditTest.jsx
+++ frontend/src/Dashboard/EditTest.jsx
@@ -17,134 +17,43 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 // 	totalGrade: { type: Number, require: true },
 // 	answers: { type: Array, require: true },
 // });
-
 function EditTest() {
 	const { id, testid, password } = useParams();
-	const [type, setType] = useState(0);
-	const [active, setActive] = useState(true);
-	const [img, setImg] = useState('');
-	const [question, setQuestion] = useState('');
-	const [totalGrade, setTotalGrade] = useState();
-	const [answers, setAnswers] = useState(new Array(4));
-	const [allQuestions, setAllQuestions] = useState([]);
-	let testHasQuestions = false;
+	const [questions, setQuestions] = useState();
+	const [testHasQuestions, setTestHasQuestions] = useState(false);
 
-	const filePicker = document.createElement('input')
-	filePicker.type = 'file';
-	filePicker.accept = 'image/*';
-	filePicker.onchange = (e) => {
-		console.log(e.target.files[0]);
-		if (e.target.files[0]) {
-			reader.readAsDataURL(e.target.files[0]);
-		}
-	}
-
-	const reader = new FileReader();
-	reader.addEventListener('loadend', () => {
-		setImg(reader.result);
-	});
 
 	useEffect(_ => {
 		getData();
 	}, [])
 
+
 	const getData = async () => {
 		const res = await axios.get(ServerAddress(`get_linkes_questions/${testid}`));
 		if (res.status === 200) {
-			setAllQuestions(res.data);
-			testHasQuestions = true;
+			setQuestions(res.data);
+			setTestHasQuestions(true);
 		} else {
-			testHasQuestions = false;
+			setTestHasQuestions(false);
 		}
 	}
 
-	const addQuestion = async () => {
-		const res = await axios.post(ServerAddress(`create/${id}/${password}/${testid}`), {
-			linkedTest: testid,
-			type: type,
-			img: img,
-			active: active,
-			question: question.trim(),
-			totalGrade: totalGrade,
-			answers: answers
-		});
-		console.log(res.data);
-		if (res.data) {
-			alert('השאלה נוספה בהצלחה !')
-			setTimeout(_ => window.location.replace(`/dashboard/edit_questions/${id}/${password}/${testid}`));
-		}
-	}
 
-	function addAnswer(currAnswer, place) {
-		setAnswers(answers[place - 1] = ({ answer: currAnswer.split(',')[0], grade: Number(currAnswer.split(',')[1]) }))
-	}
 	return (
-		<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh' }}>
-			{
-				testHasQuestions ? <div className="edit-questions">edit</div>
-					:
-					<Box
-						sx={{
-							display: 'flex',
-							justifyContent: 'center',
-							alignItems: 'center',
-							height: '100vh',
-							width: '100vw',
-						}}>
-						<Paper variant='outlined' >
-							<Typography variant='h4' sx={{ marginBottom: '20px' }}>
-								הגדרות שאלה חדשה
-							</Typography>
-							<Grid container spacing={1}>
-								<Grid item sx={12} sm={6}>
-									<FormControlLabel
-										value="end"
-										control={<Switch color="primary" onChange={e => setActive(e.target.checked)} />}
-										label="שאלה פעילה"
-										labelPlacement="end"
-									/>
-								</Grid>
-								<Grid item sx={12} sm={6}>
+		<React.Fragment>
+			<Grid columns={3}>
+				<div className="questions-menu" style={{backgroundColor: 'red'}}>
 
-									<TextField type='number' label='סוג השאלה '
-										helperText="0 לשאלה חד בחירתית ו1 לשאלה רב בחירתית" onChange={e => setType(e.target.value)} fullWidth />
-								</Grid>
+				</div>
+			</Grid>
+			<Grid columns={9}>
 
-
-								<Grid item sx={12} sm={5.6}>
-									<TextField type='text' label='שאלה' onChange={e => setQuestion(e.target.value)}
-										fullWidth />
-								</Grid>
-								<Grid item sx={12} sm={5.7}>
-									<TextField type='number' label='ציון כולל לשאלה' fullWidth onChange={e => setTotalGrade(e.target.value)} />
-								</Grid>
-
-								<Grid item sx={12} sm={10}>
-									<TextField type='text' label='תשובה וציון מופרדים בפסיק' fullWidth onChange={e => addAnswer(e.target.value, 1)} multiline rows={2} />
-									<TextField type='text' label='תשובה וציון מופרדים בפסיק' fullWidth onChange={e => addAnswer(e.target.value, 2)} multiline rows={2} />
-									<TextField type='text' label='תשובה וציון מופרדים בפסיק' fullWidth onChange={e => addAnswer(e.target.value, 3)} multiline rows={2} />
-									<TextField type='text' label='תשובה וציון מופרדים בפסיק' fullWidth onChange={e => addAnswer(e.target.value, 4)} multiline rows={2} />
-
-								</Grid>
-								<Grid>
-									<Button fullWidth size='large' sx={{ height: '100%' }} variant='outlined' onClick={e => filePicker.click()}>
-										{img ? <img src={img} width='100' height='100' /> : 'הוספת תמונה'}
-									</Button>
-								</Grid>
-								<Grid item xs={12} sx={{ textAlign: 'center' }}>
-									<Button variant='contained' onClick={e => addQuestion()}>
-										הוסף שאלה
-									</Button>
-								</Grid>
-							</Grid>
-
-
-						</Paper>
-					</Box>
-
-			}
-		</Box>
-	);
+			</Grid>
+		</React.Fragment>
+	)
 }
+
+
+
 
 export default EditTest;
